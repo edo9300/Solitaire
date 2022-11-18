c58fd906569f51ff6970f955bb453b6def8ab53f
Edoardo Lolletti <edoardo762@gmail.com>
2022-11-18 21:26:45 +0100

    Allocate/Construct GameWindow via an optional

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_main.h>
 #include <stdexcept> //std::runtime_error
-#include <memory> //std::unique_ptr
+#include <optional>
 #include "GameWindow.h"
 
 extern "C" int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
 	SDL_Init(SDL_INIT_VIDEO);
 	IMG_Init(IMG_INIT_PNG);
-	std::unique_ptr<GameWindow> game{};
+	std::optional<GameWindow> game{ std::nullopt };
 	try {
-		game = std::make_unique<GameWindow>();
+		game.emplace();
 	} catch(const std::exception& e) {
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Initialization error", e.what(), nullptr);
 		SDL_Quit();
